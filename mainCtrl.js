0a3564c78225aca21acbfe0a7c067fc2aa49c5a4
Mullen, Vernon <vernon.mullen@siriusxm.com>
2017-10-17 13:19:14 -0500

    removed revealonScroll function that caused defect in IE browser.

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,86 +1,79 @@
-angular.module('app').controller('mainCtrl', function($scope, mainSrv, $location, $anchorScroll){
+angular.module('app').controller('mainCtrl', function($scope, mainSrv, $location, $anchorScroll) {
   console.log('Enjoy your stay.');
 
 
 
-  $scope.gotoLocation = function(loc) {
-     // set the location.hash to the id of
-     // the element you wish to scroll to.
-     $location.hash(loc);
+  // $scope.gotoLocation = function(loc) {
+  // set the location.hash to the id of
+  // the element you wish to scroll to.
+  //  $location.hash(loc);
 
-     // call $anchorScroll()
-     $anchorScroll();
-   };
+  // call $anchorScroll()
+  //  $anchorScroll();
+  //  };
 
   $(".button-collapse").sideNav();
 
+  // $(function() {
+  //
+  //   var $window = $(window),
+  //     win_height_padded = $window.height() * 1.1,
+  //     isTouch = Modernizr.touch;
+  //
+  //   if (isTouch) {
+  //     $('.revealOnScroll').addClass('animated');
+  //   }
+  //
+  //   $window.on('scroll', revealOnScroll);
+  //
+  //   function revealOnScroll() {
+  //     var scrolled = $window.scrollTop(),
+  //       win_height_padded = $window.height() * 1.1;
+  //
+  //     // Showed...
+  //     $(".revealOnScroll:not(.animated)").each(function() {
+  //       var $this = $(this),
+  //         offsetTop = $this.offset().top;
+  //
+  //       if (scrolled + win_height_padded > offsetTop) {
+  //         if ($this.data('timeout')) {
+  //           window.setTimeout(function() {
+  //             $this.addClass('animated fadeInLeft ');
+  //           }, parseInt($this.data('timeout'), 20));
+  //         } else {
+  //           $this.addClass('animated fadeInLeft ');
+  //         }
+  //       }
+  //     });
+  //     // Hidden...
+  //     $(".revealOnScroll.animated").each(function(index) {
+  //       var $this = $(this),
+  //         offsetTop = $this.offset().top;
+  //       if (scrolled + win_height_padded < offsetTop) {
+  //         $(this).removeClass('animated fadeInUp shake fadeInLeft')
+  //       }
+  //     });
+  //   }
+  //
+  //   revealOnScroll();
+  // });
 
-
-
-
-  $(function() {
-
-  var $window           = $(window),
-      win_height_padded = $window.height() * 1.1,
-      isTouch           = Modernizr.touch;
-
-  if (isTouch) { $('.revealOnScroll').addClass('animated'); }
-
-  $window.on('scroll', revealOnScroll);
-
-  function revealOnScroll() {
-    var scrolled = $window.scrollTop(),
-        win_height_padded = $window.height() * 1.1;
-
-    // Showed...
-    $(".revealOnScroll:not(.animated)").each(function () {
-      var $this     = $(this),
-          offsetTop = $this.offset().top;
-
-      if (scrolled + win_height_padded > offsetTop) {
-        if ($this.data('timeout')) {
-          window.setTimeout(function(){
-            $this.addClass('animated fadeInLeft ');
-          }, parseInt($this.data('timeout'),20));
-        } else {
-          $this.addClass('animated fadeInLeft ');
-        }
-      }
-    });
-    // Hidden...
-   $(".revealOnScroll.animated").each(function (index) {
-      var $this     = $(this),
-          offsetTop = $this.offset().top;
-      if (scrolled + win_height_padded < offsetTop) {
-        $(this).removeClass('animated fadeInUp shake fadeInLeft')
-      }
-    });
-  }
-
-  revealOnScroll();
-});
-
-    $scope.collapsibleElements = [{
-        icon: 'mdi-image-filter-drama',
-        title: 'Skema',
-        stack: 'Stack: ReactJS, ExpressJS, NodeJS, ElephantSQL',
-        description: 'Description: Scheduling app for service businesses. \n Notes: Facebook Authorization, Collaboration with 3 developers, Component based'
-    },{
-        icon: 'mdi-maps-place',
-        title: 'TQKIT',
-        stack: 'Stack: AngularJS, ExpressJS, NodeJS, PostgreSQL',
-        description: 'Description: Client Management tool and work ticket system to faciliate project oversight'
-    },{
-        icon: 'mdi-social-whatshot',
-        title: 'MyReps',
-        stack: 'Stack: AngularJS, ExpressJS, NodeJS, PostgreSQL',
-        description: 'Description: Google Civic API Integration to find local, state, and congressional representatives based on location'
-    }
-];
-
-
-
-
+  $scope.collapsibleElements = [{
+    icon: 'mdi-image-filter-drama',
+    title: 'Skema',
+    stack: 'Stack: ReactJS, ExpressJS, NodeJS, ElephantSQL',
+    description: 'Description: Scheduling app for service businesses. \n Notes: Facebook Authorization, Collaboration with 3 developers, Component based'
+  }, {
+    icon: 'mdi-maps-place',
+    title: 'TQKIT',
+    stack: 'Stack: AngularJS, ExpressJS, NodeJS, PostgreSQL',
+    description: 'Description: Client Management tool and work ticket system to faciliate project oversight'
+  }, {
+    icon: 'mdi-social-whatshot',
+    title: 'MyReps',
+    stack: 'Stack: AngularJS, ExpressJS, NodeJS, PostgreSQL',
+    description: 'Description: Google Civic API Integration to find local, state, and congressional representatives based on location'
+  }];
 
 
 })
